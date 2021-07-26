81e288c993f4f4003dd76b08ed5288993244c4b7
Galatia <ptrivedius@aol.com>
2021-07-26 09:07:08 -0400

    updated to show weekday and label of Save on buttons

--- script.js
+++ script.js
@@ -1,3 +1,2 @@
-var today = moment().format("MMM Do, YYYY");
-console.log(today);
-document.getElementById("currentDay").innerHTML = today;
+var testing = moment().format('dddd MMMM Do YYYY');
+document.getElementById("testing").innerHTML = testing;
